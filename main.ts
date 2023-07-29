eb7683dfde8e0a9d2c59858b415c8ab9d974aff9
lemonchiffon <lemonchiffon@shotero.com>
2023-07-30 00:18:58 +0800

    chore: housekeeping

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 import { Application } from 'oak';
 import { router } from '@/routes/mod.ts';
-import { registerMiddlewares } from '@/middlewares.ts';
+import { registerMiddlewares } from '@/middlewares/mod.ts';
 
 const app = new Application();
 
-registerMiddlewares(app);
 app.use(router.routes());
+registerMiddlewares(app);
 
 await app.listen({ port: 8000 });
